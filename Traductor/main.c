fc9f29be185ec63a9bcefba878b7b1aa7abc733f
Melina <melinaceleg@gmail.com>
2020-04-01 14:17:47 -0300

    Add files via upload
    Debo terminar de pasar a lenguaje maquina, esta analisis de errores, split de strings, y falta ver por que no me lee la 2da linea en un file. 
    Estoy trabajando en la funcion devOperando para finalizar a lenguaje maquina, esta aun en construcción

--- Traductor/main.c
+++ Traductor/main.c
@@ -1,180 +1,305 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#define DS 2
+#define ES 3
+#define IP 4
+#define AC 8
+#define CC 9
 
-void InicializarRegistros(int reg[16],int ram[2000]);
-void LeerAsmFile();
-void Mnemonico(char ins[5],int nroinstruccion);
-void Operando(char op[8],int nroinstruccion,int reg[16]);
+#define AX 0x10
+#define BX 0x11
+#define CX 0x12
+#define DX 0x13
+#define EX 0x14
+#define FX 0x15
 
-int main(){
+
+typedef struct
+{
+    char nom[4];
+    int num;
+} Registro;
+
+typedef struct
+{
+    char cod[8];
+    char op1[4];
+    char op2[4];
+} instruccion;
+
+
+typedef struct
+{
+    char nom[11];
+    int linea;
+} Rotulo;
+
+
+
+void inicCodigos(char mnem[][4])
+{
+    strcpy(mnem[0x01],"MOV");
+    strcpy(mnem[0x02],"ADD");
+    strcpy(mnem[0x03],"SUB");
+    strcpy(mnem[0x04],"MUL");
+    strcpy(mnem[0x05],"DIV");
+    strcpy(mnem[0x06],"MOD");
+    strcpy(mnem[0x13],"CMP");
+    strcpy(mnem[0x17],"RND");
+    strcpy(mnem[0x19],"SWAP");
+    strcpy(mnem[0x20],"JMP");
+    strcpy(mnem[0x21],"JE");
+    strcpy(mnem[0x22],"JG");
+    strcpy(mnem[0x23],"JL");
+    strcpy(mnem[0x24],"JZ");
+    strcpy(mnem[0x25],"JP");
+    strcpy(mnem[0x26],"JN");
+    strcpy(mnem[0x27],"JNZ");
+    strcpy(mnem[0x28],"JNP");
+    strcpy(mnem[0x29],"JNN");
+    strcpy(mnem[0x81],"SYS");
+    strcpy(mnem[0x8F],"STOP");
+}
+
+
+void inicRegistros(Registro registros[])
+{
+    strcpy(registros[AX].nom,"AX");
+    strcpy(registros[BX].nom,"BX");
+    strcpy(registros[CX].nom,"CX");
+    strcpy(registros[DX].nom,"DX");
+    strcpy(registros[EX].nom,"EX");
+    strcpy(registros[FX].nom,"FX");
+
+}
+
+
+int comparaCodigos(char *pMnemonico, char mnem[][4], int cantCodigos)
+{
+    int enc=-1;
+    int i=1;
+    while ( i <= cantCodigos && enc == -1)
+    {
+        if (strstr(pMnemonico,mnem[i]))
+        {
+            enc = i;
+        }
+        i++;
+    }
+    return enc;
+}
+
+
+int siEsRegistro(char *op, Registro registros[])
+{
+    int i = AX;
+    int enc=-1;
+    while (i <= FX && enc==-1)
+    {
+        if (strcmp(op,registros[i].nom))
+        {
+            enc = i;
+        }
+        i++;
+    }
+
+    return enc;
+}
+
+
+int devOperando(char *op, Registro registros[])
+{
+    char operando[4]= {0x00,0x00,0x00,0x00};
+    int tipo = -1;
+    int reg;
+    int num;
+    reg=siEsRegistro(op,registros);
+    if (reg != -1)
+    {
+        tipo=0x01;
+        operando[0] = reg;
+    }
+    else
+    {
+        if (strchr(op,'[') && strchr(op,']'))
+        {
+            tipo=0x02;
+            if (strstr(op,"ES"))
+            {
+                operando[3]=ES;
+            }
+            else
+            {
+                operando[3]=DS;
+            }
+
+        }
+        else
+            tipo=0;
+    }
+//    strchr(op,'#') || strchr(op,'@') || strchr(op,'#')
+    return tipo;
+}
+
+
+void corteDatos(char *pRotulo, char *pMnemonico, char mnem[][4], int cantCodigos, Registro registros[])
+{
+    int op1=0;  ///En caso de que no posea algun operando
+//    int op2=0;
+    instruccion v;
+int indice=-1;
+    char *operando1=NULL;
+    char *operando2=NULL;
+    char *Mnemonico;
+    Mnemonico = strtok(pMnemonico," ");
+    printf("mnemonico: %s\n", Mnemonico);
+   indice = comparaCodigos(Mnemonico,mnem,cantCodigos);
+    if (indice != -1)
+    {
+        operando1 = strtok(NULL," ,");
+        printf("OPERANDO1: %s\n",operando1);
+        operando2= strtok(NULL,", ");
+        printf("operando2 %s",operando2);
+        strcpy(v.cod, mnem[indice]);
+        strcpy(v.op1,operando1);
+        strcpy(v.op2,operando2);
+        op1= devOperando(operando1,registros);
+    }
+    else
+    {
+        printf("Error, no se encontró el mnemonico\n");
+    }
+
+
+
+
+//    if (operando1)
+//        op1 = atoi(operando1);
+//    if (operando2)
+//        op2=atoi(operando2);
+
+
+}
+
+
+
+void traduccion(char *asmNOM, int reg[16], int ram[2000], Rotulo vecRotulos[], char mnem[][4], int cantCodigos, Registro registros[])
+{
+    char *assembler = (char*)malloc(sizeof(char)*128);
+    int nLinea=0;
+    Rotulo r;
+    char *pRotulo=NULL;
+    char *pRenglon = (char *)malloc(sizeof(char)*128);
+    char *pMnemonico;
+    char *p;
+    char *cadena;
+    FILE *archASM = fopen(asmNOM,"rt");
+    instruccion ins;
+    int i=0;
+
+    if (archASM)
+    {
+
+       fgets(assembler,43,archASM);
+        while (!feof(archASM))
+        {
+
+            printf("assembler: %s\n",assembler);
+            strcpy(pRenglon,assembler);
+            cadena=strtok(pRenglon,"\t\n");
+            strupr(cadena);
+            if (cadena[0] != '\0')
+            {
+                if (strstr(cadena, "//") == cadena)  ///SI ES SOLO UN COMENTARIO
+                {
+                    printf("ES SOLO UN COMENTARIO\n");
+                    cadena=strstr(cadena,"//");
+                    printf("%s", cadena);
+                }
+                else
+                {
+                    nLinea++;
+                    if (strchr(cadena,':'))
+                    {
+                        pRotulo = cadena;
+                        pMnemonico = strstr(cadena, ":");
+                        *(pMnemonico) = '\0';
+                        pMnemonico++;
+                        printf("pMnemonico %s", pMnemonico);
+                    }
+                    else
+                    {
+                        pMnemonico = cadena;
+                    }
+                    if (p=strstr(pMnemonico,"//"))
+                    {
+                        *p= '\0';
+                    }
+//            printf("assembler: %s\n",assembler);
+                    if (pRotulo != NULL)
+                    {
+                        printf("rotulo:%s\n",pRotulo);
+                        r.linea = nLinea;
+                        strcpy(r.nom,pRotulo);
+                        vecRotulos[i] = r;
+                        i++;
+                    }
+
+                    nLinea++;
+                    ///pulido en el mnemonico
+                    while (*pMnemonico == ' ')
+                        pMnemonico++;
+
+
+                   corteDatos(pRotulo,pMnemonico,mnem,cantCodigos,registros);
+                }
+            }
+           fgets(assembler,43,archASM);
+        }
+
+
+    }
+    fclose(archASM);
+}
+
+
+
+int main(int argc, char *argv[])
+{
     int reg[16];
     int ram[2000];
-    void InicializarRegistros(reg,ram);
-    void LeerAsmFile();
-    return 0;
-}
+    char mnem[144][4];
+    Rotulo vecRotulos[10];
+    int cantCodigos=0x8F;
+    Registro registros[16];
+    inicRegistros(registros);
 
-void InicializarRegistros(int reg[16],int ram[2000]){
+    char asmNOM[] = "archASM.asm";
+    char *imgNOM;
     int i;
-    for(i=0;i<16;i++){
-        reg[i]=0x0000;
-    }
-    for(i=0;i<2000;i++){
-        ram[i]=0x0000;
-    }
-    reg[2]=500;     //DS empieza en la posicion 500 de RAM
-    reg[3]=1000;        //ES empieza en la posicion 1000 de RAM
-}
+    char *comando;
+    traduccion(asmNOM,reg,ram,vecRotulos,mnem,cantCodigos,registros);
 
-void LeerAsmFile(){
-    FILE *asmFile = fopen("d:\\test.txt", "rt");
-    char cadena[20],texto[20];
-    char c;
-    int indice = 0,nrolinea=0;
-	int ins,op1,op2,i,j;
-	ins=op1=op2=0; //*Para controlar que se cargo
 
-	fscanf(asmFile,"%c",&c);
-	while(!feof(asmFile)){
-        i=0; nrolinea++;
-        while(c!='\n'){     //Copia una linea del archivo en 'texto'
-            texto[i++]=c;
-            fscanf(asmFile,"%c",&c);
-        }
-        i=0;
-        while(texto[i]==' '){ //Salta los espacios que hay antes del mnemonico
-            i++;
-        }
-        c=texto[i];
-        while(i<strlen(texto) && c!='/'){  //Pasa a la siguiente linea cuando haya terminado o haya un comentario
-            while(c!=' '){      //Copia en 'cadena' una parte de 'texto'
-                if(c==',' || c=='\t')
-                    c=' ';
-                else{
-                    cadena[indice++]=c;
-                    c=texto[++i];
-                }
-            }
-            cadena[indice]='\0';
-            if (cadena[0]!='\0'){
-                if(ins==0){
-                    for(j=0;j<strlen(cadena);j++)
-                        cadena[j]=toupper(cadena[j]);
-                    //Carga el mnemonico
-                    ins=1;
-                }
-                else{
-                    if(op1==0){
-                        //Carga el primer operando
-                        op1=1;
-                    }
-                    else{
-                        //*Carga el seundo operando
-                        op2=1;
-                    }
-                }
-            }
-        indice=0;
-        c=texto[i];
-        }
-        ins=op1=op2=0;
-        fscanf(asmFile,"%c",&c);
-    }
-    fclose(asmFile);
-}
-
-void Mnemonico(char ins[5],int nroinstruccion){
-    int cod;
-    if(!strcmp(ins,"MOV"))
-        cod=0x01;
-    if(!strcmp(ins,"ADD"))
-        cod=0x02;
-    if(!strcmp(ins,"SUB"))
-        cod=0x03;
-    if(!strcmp(ins,"MUL"))
-        cod=0x04;
-    if(!strcmp(ins,"DIV"))
-        cod=0x05;
-    if(!strcmp(ins,"MOD"))
-        cod=0x06;
-    if(!strcmp(ins,"CMP"))
-        cod=0x13;
-    if(!strcmp(ins,"SWAP"))
-        cod=0x17;
-    if(!strcmp(ins,"RND"))
-        cod=0x19;
-    if(!strcmp(ins,"AND"))
-        cod=0x31;
-    if(!strcmp(ins,"OR"))
-        cod=0x32;
-    if(!strcmp(ins,"NOT"))
-        cod=0x33;
-    if(!strcmp(ins,"XOR"))
-        cod=0x34;
-    if(!strcmp(ins,"SHL"))
-        cod=0x37;
-    if(!strcmp(ins,"SHR"))
-        cod=0x38;
-    if(!strcmp(ins,"JMP"))
-        cod=0x20;
-    if(!strcmp(ins,"JE"))
-        cod=0x21;
-    if(!strcmp(ins,"JG"))
-        cod=0x22;
-    if(!strcmp(ins,"JL"))
-        cod=0x23;
-    if(!strcmp(ins,"JZ"))
-        cod=0x24;
-    if(!strcmp(ins,"JP"))
-        cod=0x25;
-    if(!strcmp(ins,"JN"))
-        cod=0x26;
-    if(!strcmp(ins,"JNZ"))
-        cod=0x27;
-    if(!strcmp(ins,"JNP"))
-        cod=0x28;
-    if(!strcmp(ins,"JNN"))
-        cod=0x29;
-    if(!strcmp(ins,"SYS"))
-        cod=0x81;
-    if(!strcmp(ins,"STOP"))
-        cod=0x8F;
-}
-void Operando(char op[8],int nroinstruccion,int reg[16]){
-    int tipo,val,i,j; char aux[3];
-    if(!strcmp(op,"AX")){
-        tipo=0x01; val=0xA;
-    }if(!strcmp(op,"BX")){
-        tipo=0x01; val=0xB;
-    }if(!strcmp(op,"CX")){
-        tipo=0x01; val=0xC;
-    }if(!strcmp(op,"DX")){
-        tipo=0x01; val=0xD;
-    }if(!strcmp(op,"EX")){
-        tipo=0x01; val=0xE;
-    }if(!strcmp(op,"FX")){
-        tipo=0x01; val=0xF;
-    }
-    if(op[0]=='['){
-        tipo=0x02; j=0;
-        if(op[3]==':'){
-            aux[0]=op[1];aux[1]=op[2];
-            if(!strcmp(aux,"DS"))
-                val=reg[2];
-            if(!strcmp(aux,"ES"))
-                val=reg[3];
-            i=4;
-        }
-        else{
-            val=0;i=1;
-        }
-        while(op[i]!=']'){
-            aux[j]=op[i];
-        }
-        val+=atoi(aux);
-    }
-    else{
-        val=atoi(op);
-    }
+//    for (int i = 0 ; i< argc; i++)
+//    {
+//        if (strstr(argv[i], ".asm"))
+//        {
+//            asmNOM= (char *)malloc(strlen(argv[i])*sizeof(char));
+//            asmNOM = argv[i];
+//            traduccion(asmNOM,reg,ram,vecRotulos);
+//        }
+//        if (strstr(argv[i], ".img"))
+//        {
+//            imgNOM = (char*)malloc(strlen(argv[i])*sizeof(char));
+//            imgNOM = argv[i];
+//        }
+//        if (strstr(argv[i], "-"))
+//        {
+//            comando = (char*)malloc(strlen(argv[i])*sizeof(char));
+//            comando = argv[i];
+//        }
+//    }
+    return 0;
 }
